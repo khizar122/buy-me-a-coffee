d7a69b82855cd7f55e2460afceba168cd81d614f
unknown <samman.urooj@datics.ai>
2025-05-11 00:42:11 +0500

    coing soon impemtaion

---
+++ app/(protected)/comingSoon/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import ComingSoonPage from "./comingSoonCard";
+
+
+
+export default function MainComingSoonPage() {
+  return (
+    <ComingSoonPage
+      title="Coming Soon"
+      description="Soon you'll be able to discover."
+     
+      redirectPath="/dashboard"
+    />
+  );
+}
